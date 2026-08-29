f36693c7dd22035de14af912f5d0de8205f10ca9
Lily <hadung29112009@gmail.com>
2026-08-29 13:21:23 +0700

    fixing redirecting to post and club detail pages

--- amsclubhub-frontend/src/app/(main)/clubs/[id]/page.tsx
+++ amsclubhub-frontend/src/app/(main)/clubs/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 
 import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
--- amsclubhub-frontend/src/app/(main)/posts/[id]/page.tsx
+++ amsclubhub-frontend/src/app/(main)/posts/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 
 import { useState, useEffect, use } from 'react';
 import Link from 'next/link';
